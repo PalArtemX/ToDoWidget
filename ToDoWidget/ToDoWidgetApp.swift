b063c51a995ec59fa57ee32db47a4bfbb3d5dbae
Artem Paliutin <pal.artem@icloud.com>
2022-01-22 19:26:37 +0300

    Initial Commit

---
+++ ToDoWidget/ToDoWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoWidgetApp.swift
+//  ToDoWidget
+//
+//  Created by Artem Paliutin on 22.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDoWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
